0deebec2ec6a69514b6c9dab206ef11c50e453e7
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2025-10-03 14:39:58 +0200

    (refactor): parse PR status into enum

--- src/prstack/vcs.clj
+++ src/prstack/vcs.clj
@@ -198,25 +198,44 @@
     ["gh" "pr" "merge" pr-number]
     {:echo? true}))
 
+(def ^:lsp/allow-unused PR
+  [:map
+   [:pr/title :string]
+   [:pr/number :int]
+   [:pr/url :string]
+   [:pr/status
+    [:enum
+     :pr.status/approved
+     :pr.status/changes-requested
+     :pr.status/review-required
+     :pr.status/unknown
+     :pr.status/other]]])
+
+(defn- parse-pr [json-output]
+  (when json-output
+    {:pr/title (:title json-output)
+     :pr/number (:number json-output)
+     :pr/url (:url json-output)
+     :pr/status (case (:reviewDecision json-output)
+                  "APPROVED" :pr.status/approved
+                  "CHANGES_REQUESTED" :pr.status/changes-requested
+                  "REVIEW_REQUIRED" :pr.status/review-required
+                  (if (nil? (:reviewDecision json-output))
+                    :pr.status/unknown
+                    :pr.status/other))}))
+
+;; Maybe consider fetching all PRs at once and matching them on branches.
 (defn find-pr
   [head-branch base-branch]
   (spit "target/dev.log" (str "Fetching PR for " head-branch " onto " base-branch "\n") :append true)
-  (some->
+  (parse-pr
     (json/read-str
       (not-empty
         (u/run-cmd ["gh" "pr" "list"
                     "--head" head-branch
                     "--base" base-branch
                     "--limit" "1"
-                    "--json" "title,number,url,reviewDecision" "--jq" ".[0]"])))
-    (update-keys #(keyword "pr" (name %)))
-    (update :pr/reviewDecision
-      #(case %
-         "APPROVED" :pr.status/approved
-         "CHANGES_REQUESTED" :pr.status/changes-requested
-         "REVIEW_REQUIRED" :pr.status/review-required
-         nil))
-    (set/rename-keys {:pr/reviewDecision :pr/status})))
+                    "--json" "title,number,url,reviewDecision" "--jq" ".[0]"])))))
 
 (defn config
   "Reads the VCS configuration"
