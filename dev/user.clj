11e0ba6b84ed7ff222190585c03b3a855cc4b997
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2026-01-23 07:16:15 +0100

    (refactor): further cleanups

--- dev/user.clj
+++ dev/user.clj
@@ -3,7 +3,8 @@
     [portal.api :as p]
     [prstack.config :as config]
     [prstack.stack :as stack]
-    [prstack.vcs :as vcs]))
+    [prstack.vcs :as vcs]
+    [prstack.vcs.graph :as vcs.graph]))
 
 (require 'hashp.preload)
 
@@ -25,4 +26,12 @@
   (def vcs- (vcs/make config-))
   (stack/get-current-stacks vcs- config-)
   (stack/get-all-stacks vcs- config-))
+
+  ;; Deeper
+  (def vcs-graph- (vcs/read-current-stack-graph vcs-))
+  (def current-id- (vcs/current-change-id vcs-))
+  (def paths- (vcs.graph/find-all-paths-to-trunk vcs-graph- current-id-))
+  (map #(stack/path->stack vcs-graph- % config-) paths-)
+  (map #(vcs.graph/get-node vcs-graph- %) (first paths-))
+
   )
