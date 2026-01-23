0afe88a395550b831c12679cb4eb87eb9424bae1
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2026-01-23 08:08:40 +0100

    (feat): move documentation app to own repo

--- documentation/app/(home)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from 'react';
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '../layout.shared';
-
-export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
-}
