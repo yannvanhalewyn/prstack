3e22737597e27a6a94e49a8e9b3b0ea0d9bac9fa
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2026-01-23 08:08:40 +0100

    (feat): move documentation app to own repo

--- documentation/lib/source.ts
+++
@@ -1,8 +0,0 @@
-import { docs, meta } from '@/.source/server';
-import { loader } from 'fumadocs-core/source';
-import { toFumadocsSource } from 'fumadocs-mdx/runtime/server';
-
-export const source = loader({
-  baseUrl: '/docs',
-  source: toFumadocsSource(docs, meta),
-});
